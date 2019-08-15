0d6a899ff1398a27dd96e5b270d867fa90049f8d
Rikuto Hayashi <ritu6811@gmail.com>
2019-08-15 22:45:11 +0900

    5-2

--- index.php
+++ index.php
@@ -1,27 +1,39 @@
 <!doctype html>
 <html lang="ja">
 <head>
-<!-- Required meta tags -->
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<!-- Bootstrap CSS -->
-<link rel="stylesheet" href="css/style.css">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/style.css">
 
-<title>よくわかるPHPの教科書</title>
+  <title>よくわかるPHPの教科書</title>
 </head>
 <body>
-<header>
-<h1 class="font-weight-normal">トップページです。</h1>
-</header>
+  <header>
+    <h1 class="font-weight-normal">トップページです。</h1>
+  </header>
 
-<main>
-<h2>Practice</h2>
-<pre>
-<?php
-/* ここに、PHPのプログラムを記述します */
-?>
-</pre>
-</main>
+  <main>
+    <h2>Practice</h2>
+    <pre>
+      <?php
+      /* ここに、PHPのプログラムを記述します */
+      try{
+        $db = new PDO('mysql:dbname=mydb;
+        host=localhost;
+        charset=utf8',//utf-8ではなくutf8
+        'root',
+        '');
+      }catch(PDOException $e){
+        echo 'DB接続エラー:'. $e -> getMessage();
+      }
+
+      $count = $db->exec('INSERT INTO my_items SET maker_id=1, item_name="もも",price=210,keyword="缶詰,ピンク,甘い",sales=0, created="2018-01-23",modified="2018-01-23"');
+      echo $count.'件のデータを挿入しました。';
+      ?>
+    </pre>
+  </main>
 </body>
 </html>
